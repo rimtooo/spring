cff94c6c9fceb5df15d6a150a8481b856069b23a
admin <admin@DESKTOP-Q5P6R0R>
2024-09-05 14:54:34 +0900

    0905

---
+++ SpringProject/src/main/java/com/yedam/java/emp/mapper/EmpMapper.java
@@ -0,0 +1,9 @@
+package com.yedam.java.emp.mapper;
+
+import java.util.List;
+
+import com.yedam.java.emp.service.EmpVO;
+
+public interface EmpMapper {
+	public List<EmpVO> selectEmpAll();
+}
---
+++ SpringProject/src/main/java/com/yedam/java/emp/service/EmpVO.java
@@ -0,0 +1,44 @@
+package com.yedam.java.emp.service;
+
+import java.util.Date;
+
+public class EmpVO {
+	private Integer employeeId;
+	private String lastName;
+	private String email;
+	private Date hireDate;
+	private String jobId;
+	
+	public Integer getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(Integer employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Date getHireDate() {
+		return hireDate;
+	}
+	public void setHireDate(Date hireDate) {
+		this.hireDate = hireDate;
+	}
+	public String getJobId() {
+		return jobId;
+	}
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+	}
+	
+	
+}
